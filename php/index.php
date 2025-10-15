5e63f6590639b495638a9270fd0da0649602950c
AntoineLD <119339278+AntoineLD@users.noreply.github.com>
2025-10-15 12:17:39 +0200

    .

--- php/index.php
+++ php/index.php
@@ -3,14 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FreshLeg</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Vente en ligne de légumes frais et locaux.">
     
     <title>FreshLeg - Vos légumes frais en ligne</title>
@@ -62,30 +54,34 @@
                             <button class="add-to-cart-button">Ajouter au panier</button>
                         </div>
                     </article>
+                    
                     <article class="product-card">
-                        <img src="assets/img/haricots_verts.png" alt="Grappe de tomates bio juteuses">
+                        <img src="assets/img/haricots_verts.png" alt="Haricots verts frais">
                         <div class="card-content">
                             <h4>Haricots verts Bio</h4>
                             <p class="product-price">2.50€ / kg</p>
                             <button class="add-to-cart-button">Ajouter au panier</button>
                         </div>
                     </article>
+                    
                     <article class="product-card">
-                        <img src="assets/img/haricots_blancs.png" alt="Grappe de tomates bio juteuses">
+                        <img src="assets/img/haricots_blancs.png" alt="Haricots blancs secs">
                         <div class="card-content">
                             <h4>Haricots</h4>
                             <p class="product-price">2.00€ / kg</p>
                             <button class="add-to-cart-button">Ajouter au panier</button>
                         </div>
                     </article>
+                    
                     <article class="product-card">
-                        <img src="assets/img/céleri.png" alt="Grappe de tomates bio juteuses">
+                        <img src="assets/img/céleri.png" alt="Branche de céleri fraîche">
                         <div class="card-content">
-                            <h4>Branche de céleris Bio</h4>
-                            <p class="product-price">1.00€ / branches</p>
+                            <h4>Branche de céleri Bio</h4>
+                            <p class="product-price">1.00€ / branche</p>
                             <button class="add-to-cart-button">Ajouter au panier</button>
                         </div>
                     </article>
+
                 </div>
             </div>
         </section>
@@ -95,9 +91,3 @@
 
 </body>
 </html>
-
-
-
-</body>
-</html>
-
