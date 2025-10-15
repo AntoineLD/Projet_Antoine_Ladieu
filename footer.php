4b8398154f8ab93dbb2e91135d32c7c2064f8c20
AntoineLD <119339278+AntoineLD@users.noreply.github.com>
2025-10-15 11:11:56 +0200

    Mise en place d'une page html, un footer et css

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php?>
+<footer class="site-footer">
+    <div class="container">
+        <div class="footer-nav">
+            <h4>Liens utiles</h4>
+            <ul>
+                <li><a href="#">Mentions Légales</a></li>
+                <li><a href="#">Conditions Générales de Vente</a></li>
+                <li><a href="#">Contactez-nous</a></li>
+            </ul>
+        </div>
+        <div class="footer-social">
+            <h4>Suivez-nous</h4>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Twitter</a></li>
+            </ul>
+        </div>
+        <p class="copyright">&copy; <?php echo date('Y'); ?> FreshVeg. Tous droits réservés.</p>
+    </div>
+</footer>
+
+</body>
+</html>
